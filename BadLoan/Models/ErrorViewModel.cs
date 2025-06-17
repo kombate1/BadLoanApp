7173f55e102d812469206c5d064cdc1a3626f5b0
kombate1 <bikinankombate1@gmail.com>
2025-06-17 09:47:44 +0000

    Add project files.

---
+++ BadLoan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BadLoan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

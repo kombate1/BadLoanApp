fbd680174b2e87f9cd9bc477f60bd59bb533c24e
kombate1 <bikinankombate1@gmail.com>
2025-06-17 11:19:00 +0000

    Added Customer model and defaultConnection Strings

---
+++ BadLoan/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BadLoan.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Occupation { get; set; }
+        [Required]
+        public string EmployerName { get; set; }
+        [Required]
+        public string EmployerContact { get; set; }
+        [Required]
+        public decimal YearlyIncome { get; set; }
+        [Required]
+        public string HomeAddress { get; set; }
+        [Required]
+        public string PlaceOfWork { get; set; }
+    }
+}

4fd6a830ae416d2539215486d7eab7e4bea4f8db
kombate1 <bikinankombate1@gmail.com>
2025-06-26 11:44:03 +0000

    Created the calculator app with necessary calculations and a brief note for eligibilty

---
+++ BadLoan/Controllers/CalculatorController.cs
@@ -0,0 +1,54 @@
+﻿
+using BadLoan.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BadLoan.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator c)
+        {
+            // Assuming c.AnnualIncome is a decimal and you want to calculate interest based on input from the Calculator model in the view
+            decimal interest = 0;
+
+            if(c.LoanType == "Personal")
+            {
+                interest = c.LoanAmount * 0.15m; // Example calculation for personal loan
+            }
+            else if(c.LoanType == "Home")
+            {
+                interest = c.LoanAmount * 0.25m; // Example calculation for home loan
+            }
+            else if(c.LoanType == "Auto")
+            {
+                interest = c.LoanAmount * 0.20m; // Example calculation for car loan
+            }
+
+            decimal amountToPayMthly = (c.LoanAmount / c.Duration) + interest;
+
+            decimal debtServiceRatio = (amountToPayMthly / c.MonthlyIncome) * 100;
+
+            ViewBag.Message = debtServiceRatio;
+
+            if (debtServiceRatio > 40)
+            {
+                ViewBag.Message = "You are not eligible for this loan";
+            }
+            else
+            {
+                ViewBag.Message = "You are eligible for this loan";
+            }
+
+            return View(c);
+        }
+
+        
+    }
+}
---
+++ BadLoan/Models/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace BadLoan.Models
+{
+    public class Calculator
+    {
+        public decimal MonthlyIncome { get; set; }
+        public decimal LoanAmount { get; set; }
+        public string LoanType { get; set; }
+        public int Duration { get; set; }
+    }
+}

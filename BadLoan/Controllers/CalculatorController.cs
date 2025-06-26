2431fea64c08cbf5363e0741e2683ec01f0e0230
kombate1 <bikinankombate1@gmail.com>
2025-06-26 17:08:06 +0000

    Added the display message for eligibility after calculation

--- BadLoan/Controllers/CalculatorController.cs
+++ BadLoan/Controllers/CalculatorController.cs
@@ -1,6 +1,8 @@
 ﻿
 using BadLoan.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Model;
 
 
 namespace BadLoan.Controllers
@@ -15,6 +17,34 @@ namespace BadLoan.Controllers
         [HttpPost]
         public IActionResult Index(Calculator c)
         {
+
+            // Server-side duration validation based on loan type
+            int min = 1, max = 30;
+            switch (c.LoanType?.ToLower())
+            {
+                case "personal":
+                    max = 5;
+                    break;
+                case "auto":
+                    max = 7;
+                    break;
+                case "mortgage":
+                    min = 5;
+                    max = 30;
+                    break;
+            }
+
+            if (c.Duration < min ||c.Duration > max)
+            {
+                ModelState.AddModelError("Duration", $"Duration for {c.LoanType} must be between {min} and {max} years.");
+                //ModelState.AddModelError("Duration", $"Duration for {c.LoanType} must be between {min} and {max} years.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(c);
+            }
+
             // Assuming c.AnnualIncome is a decimal and you want to calculate interest based on input from the Calculator model in the view
             decimal interest = 0;
 
@@ -37,14 +67,41 @@ namespace BadLoan.Controllers
 
             ViewBag.Message = debtServiceRatio;
 
+
             if (debtServiceRatio > 40)
             {
-                ViewBag.Message = "You are not eligible for this loan";
+                ViewBag.MessageHtml = @"
+        <div class='card text-white bg-danger mt-3'>
+            <div class='card-header'>Loan Status</div>
+            <div class='card-body'>
+                <h5 class='card-title'>Not Eligible</h5>
+                <p class='card-text'>You are not eligible for this loan. Please reduce your loan request amount.</p>
+            </div>
+        </div>";
             }
             else
             {
-                ViewBag.Message = "You are eligible for this loan";
+                ViewBag.MessageHtml = @"
+        <div class='card text-white bg-success mt-3'>
+            <div class='card-header'>Loan Status</div>
+            <div class='card-body'>
+                <h5 class='card-title'>Eligible</h5>
+                <p class='card-text'>Congratulations! You are eligible for this loan.</p>
+            </div>
+        </div>";
             }
+
+           
+
+
+            //if (debtServiceRatio > 40)
+            //{
+            //    ViewBag.Message = "You are not eligible for this loan";
+            //}
+            //else
+            //{
+            //    ViewBag.Message = "You are eligible for this loan";
+            //}
 
             return View(c);
         }
--- BadLoan/Models/Calculator.cs
+++ BadLoan/Models/Calculator.cs
@@ -1,8 +1,12 @@
-﻿namespace BadLoan.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BadLoan.Models
 {
     public class Calculator
     {
+        [Range(1,10000000000)]
         public decimal MonthlyIncome { get; set; }
+        [Range(1, 10000000000)]
         public decimal LoanAmount { get; set; }
         public string LoanType { get; set; }
         public int Duration { get; set; }

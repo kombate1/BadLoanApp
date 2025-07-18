65913f552517a8951ef7b295f1e898fd2ace731d
kombate1 <bikinankombate1@gmail.com>
2025-07-18 13:40:51 +0000

    Role based access, upload documents based on file type and download them

---
+++ BadLoan.Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadLoan.Utility
+{
+    public static class SD
+    {
+        public const string role_approvalManager = "Approval Manager";
+        public const string role_User_Customer = "Customer";
+    }
+}

5fe43e86ea1934685887a6fd2e014f00a7f220a4
kombate1 <bikinankombate1@gmail.com>
2025-08-04 20:24:23 +0000

    Created the notification page

--- BadLoan/Models/Notification.cs
+++ BadLoan/Models/Notification.cs
@@ -13,6 +13,7 @@ namespace BadLoan.Models
         public string UserId { get; set; }
         public string? Email { get; set; }
         public string? Message { get; set; }
+        public bool IsRead { get; set; } = false;
         public DateTime Timestamp { get; set; } = DateTime.Now;
 
     }

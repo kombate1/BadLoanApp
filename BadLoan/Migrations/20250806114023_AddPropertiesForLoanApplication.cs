2602b411f3de8d6a052ec49260eaf9905be9c30e
kombate1 <bikinankombate1@gmail.com>
2025-08-06 14:00:08 +0000

    Added the dsr and Monthly repayment to the db and notifications to the admin page, also the calculator no longer reroutes

---
+++ BadLoan/Migrations/20250806114023_AddPropertiesForLoanApplication.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BadLoan.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesForLoanApplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "debtServiceRatio",
+                table: "LoanApplications",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "monthlyRepayment",
+                table: "LoanApplications",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "debtServiceRatio",
+                table: "LoanApplications");
+
+            migrationBuilder.DropColumn(
+                name: "monthlyRepayment",
+                table: "LoanApplications");
+        }
+    }
+}
